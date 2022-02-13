f38dbdd67d8f59ac4c5b23d59b27b257fcb0ecf6
2012wuguang <wuguang_123@163.com>
2022-02-13 20:57:08 +0800

    update

--- algorithm/java-test/BinaryTreeMy.java
+++ algorithm/java-test/BinaryTreeMy.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class BinaryTreeMy {
@@ -60,6 +61,62 @@ public class BinaryTreeMy {
         inOrderRecurs(head.right);
     }
 
+    public static void postOrderRecurs(Node head){
+        if(head == null){
+            return;
+        }
+        postOrderRecurs(head.left);
+        postOrderRecurs(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void postOrderIteration(Node head){
+        if(head == null){
+            return;
+        }
+        Stack<Node> s = new Stack<>();
+        Stack<Node> reversStack = new Stack<>();
+        s.push(head);
+
+        while(!s.isEmpty()  ){
+            Node cur = s.pop();
+            reversStack.push(cur);
+            if(cur.left != null){
+                s.push(cur.left);
+            }
+            if(cur.right != null){
+                s.push(cur.right);
+            }
+        }
+
+        while(!reversStack.isEmpty()){
+            System.out.print(reversStack.pop().value + " ");
+        }
+    }
+
+    //层次遍历
+    public static void hierarchicalTraversal(Node head){
+        if(head == null){
+            return;
+        }
+        LinkedList<Node> curQueue = new LinkedList<>();
+        LinkedList<Node> nextQueue = new LinkedList<>();
+        curQueue.add(head);
+        while(!curQueue.isEmpty()){
+            Node cur = curQueue.pop(); 
+            System.out.print(cur.value + " ");
+            if(cur.left != null){
+                nextQueue.add(cur.left);
+            }
+            if(cur.right != null){
+                nextQueue.add(cur.right);
+            }
+            if( curQueue.isEmpty() && !nextQueue.isEmpty()){
+                //进入一行进行计算
+                curQueue = nextQueue;
+            }
+        }
+    }
 
     public static void main(String [] args){
         Node tree = new Node(1);
@@ -75,14 +132,13 @@ public class BinaryTreeMy {
         tree.left.left.right.left.left.right = new Node(10);
         tree.left.left.right.left.left.right.right = new Node(11);
         
+     
         /*
         preOrderRecurs(tree);
-
         System.out.println( " " );
         preOrderIteration(tree);
-
         System.out.println( " " );
-        */
+    
         System.out.println( "递归版中序遍历:" );
         inOrderRecurs(tree);
 
@@ -90,5 +146,162 @@ public class BinaryTreeMy {
 
         System.out.println( "迭代版中序遍历:" );
         inOrderIteration(tree);
+        
+        
+        System.out.println( "递归版后序遍历:" );
+        postOrderRecurs(tree);
+
+        System.out.println(" ");
+
+        System.out.println( "迭代版后序遍历:" );
+        postOrderIteration(tree);
+       
+
+        System.out.println( "BFS-层次遍历:" );
+        hierarchicalTraversal(tree);
+         
+        */
+        
+        System.out.println( "postOrderIteration 遍历:" );
+        postOrderIteration(tree);
+        System.out.println( "" );
+        System.out.println( "postOrderRecurs 遍历:" );
+        postOrderRecurs(tree);
+
+        System.out.println( "" );
+        System.out.println( "morrisForPost 遍历:" );
+        morrisForPost(tree);
+        System.out.println( "" );
+
     }
+
+    
+    public static void morrisForPost(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node cur = head;
+        while(cur != null){
+            Node leftNode = cur.left;
+            if(leftNode != null){
+                Node rightEstNode = leftNode;
+                while(rightEstNode.right != null && rightEstNode.right != cur){
+                    rightEstNode = rightEstNode.right;
+                }
+
+                if(rightEstNode.right == null){
+                    rightEstNode.right = cur;
+                  
+                    cur = cur.left;
+                }else{
+                    rightEstNode.right = null;
+                    
+                    printLeftEdge(leftNode);
+                    //第二来到 cur
+                    cur = cur.right;
+                }
+            }else{
+                cur = cur.right;
+            }
+        }
+        printLeftEdge(head);
+
+    }
+
+    public static void printLeftEdge(Node leftNode){
+        if(leftNode == null){
+            return;
+        }
+        leftNode = reverseNode(leftNode);
+        while(leftNode != null){
+            System.out.print(leftNode.value + " ");
+            leftNode = leftNode.right;
+        }
+        reverseNode(leftNode);
+    }
+
+    public static Node reverseNode(Node node){
+        Node pre = null;
+        Node cur = node;
+        while(cur != null){
+            Node next = cur.right;
+            cur.right = pre;
+
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+
+
+    public static void morrisForIn(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node cur = head;
+        while(cur != null){
+            Node leftNode = cur.left;
+            if(leftNode != null){
+                Node rightEstNode = leftNode;
+                while(rightEstNode.right != null && rightEstNode.right != cur){
+                    rightEstNode = rightEstNode.right;
+                }
+
+                if(rightEstNode.right == null){
+                    rightEstNode.right = cur;
+                  
+                    cur = cur.left;
+                }else{
+                    rightEstNode.right = null;
+                    // 第一次
+                    System.out.print( cur.value + " ");
+                    //第二来到 cur
+                    cur = cur.right;
+                }
+            }else{
+                // 
+                System.out.print( cur.value + " ");
+                cur = cur.right;
+            }
+        }
+    }
+
+
+
+    public static void morrisForPre(Node head){
+        if(head == null){
+            return;
+        }
+
+        Node cur = head;
+        while(cur != null){
+            Node leftNode = cur.left;
+            if(leftNode != null){
+                Node rightEstNode = leftNode;
+                while(rightEstNode.right != null && rightEstNode.right != cur){
+                    rightEstNode = rightEstNode.right;
+                }
+
+                if(rightEstNode.right == null){
+                    rightEstNode.right = cur;
+                    // 第一次
+                    System.out.print( cur.value + " ");
+                    cur = cur.left;
+                }else{
+                    rightEstNode.right = null;
+                    //第二来到 cur
+                    cur = cur.right;
+                }
+            }else{
+                // 
+                System.out.print( cur.value + " ");
+                cur = cur.right;
+            }
+        }
+    }
+
+
 }
