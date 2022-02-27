fb33fb9c186e70ef08b255c3b9b36be5309da475
wuguang <wuguang_123@163.com>
2022-02-27 20:40:26 +0800

    update shellsort

--- algorithm/done/javaFold/Sort.java
+++ algorithm/done/javaFold/Sort.java
@@ -51,6 +51,35 @@ public class Sort {
         for(int i=1;i<=len-1;i++){
             minIndex = i;
             //需要比较的数的索引
+            //需要一个可以终止的循环
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]>arr[minIndex]){
+                    //交换最小值和j的索引
+                    swap(arr, j, minIndex);
+                    minIndex = j;
+                }else{
+                    //如果有一个满足条件，那么后面的都满足条件，需终止
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+
+        //插入排序
+    //任务前面是排好序的，找到合适的地方，插入进去
+    public static int [] insertionSort02(int[] arr){
+        if(arr.length<=1){
+            return arr;
+        }
+        int len = arr.length;
+        int minIndex = -1;
+        //[0,n-2]
+        for(int i=1;i<=len-1;i++){
+            minIndex = i;
+            //需要比较的数的索引
             //需要一个可以终止的循环,选while 
             for(int j=i-1;j>=0;j--){
                 if(arr[j]>arr[minIndex]){
@@ -63,6 +92,31 @@ public class Sort {
         return arr;
     }
 
+
+    //希尔排序
+    //第一个突破O(n^2)的算法
+    public static int[] shellSort(int [] arr){
+        if(arr.length<=1){
+            return arr;
+        }
+        int len = arr.length;
+        //i,j,k
+        for(int gap=len/2;gap>0;gap/=2){
+            //从第二个元素开始
+            for(int i=gap;i<=len-2;i+=gap){
+                //arr[i]
+                int minIndex = i;
+                int j = i-gap;
+                while(arr[j]>arr[minIndex] && j>=0){
+                    swap(arr,minIndex,j);
+                    minIndex = j;
+                    j -= gap;
+                }
+            }
+        }
+
+        return arr;
+    }
 
 
     public static void swap(int[] arr,int i,int j){
